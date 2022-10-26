029fd7927964befe59f801f38ce20749c81de43a
Achyutha Reddy <s550301@nwmissouri.edu>
2022-10-26 17:34:44 -0500

    updated the all functions in bonus file

--- routes/computation.js
+++ routes/computation.js
@@ -5,7 +5,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     var ranval = Math.floor(Math.random() * 10);
     var ranval2 = Math.floor(Math.random()*20);
-    res.render('computation', { title: 'Achyutha reddy\'s computaion page', results:`Math.imul(x, y)  applied to ${ranval}, ${ranval2} is ${Math.imul(ranval, ranval2)}`});
+    res.render('computation', { title: 'Achyutha reddy\'s computaion page', 
+        result1:`Math.imul(x, y)  applied to ${ranval}, ${ranval2} is ${Math.imul(ranval, ranval2)}`,
+         result2: `Math.log(x)  applied to ${ranval} is ${Math.log(ranval)}`,
+         result3: `Math.log10(x)  applied to ${ranval2} is ${Math.log10(ranval2)}`,
+
+
+    });
 
 });
 
