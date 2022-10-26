511efcf3bd54f2c9878836430c2531f6c6f776c9
Achyutha Reddy <s550301@nwmissouri.edu>
2022-10-26 17:17:26 -0500

    commiting the bonus page

--- routes/computation.js
+++ routes/computation.js
@@ -3,9 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    var ranval = Math.floor(Math.random());
-    var ranval2 = Math.floor(Math.random());
-    res.render('computation', { title: 'Achyutha reddy\'s computaion page', result:`Math.imul(x, y)  applied to ${ranval, ranval2} is ${Math.expm1(ranval, ranval2)}`});
+    var ranval = Math.floor(Math.random() * 10);
+    var ranval2 = Math.floor(Math.random()*20);
+    res.render('computation', { title: 'Achyutha reddy\'s computaion page', results:`Math.imul(x, y)  applied to ${ranval}, ${ranval2} is ${Math.imul(ranval, ranval2)}`});
 
 });
 
