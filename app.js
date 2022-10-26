7f1da43d68f8d2fbf0209ad24c29b3f1300e0301
Achyutha Reddy <s550301@nwmissouri.edu>
2022-10-26 16:55:40 -0500

    updated users js file to git

--- app.js
+++ app.js
@@ -25,12 +25,12 @@ app.use('/users', usersRouter);
 app.use('/mydata', mydataRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -38,10 +38,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
-
-const server = app.listen(process.env.PORT || 3000, () => {
-  console.log(`${server.address().port}`);
 });
 
 module.exports = app;
